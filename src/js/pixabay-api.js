3bc4f94e0cd2e24229eca052b842a0b667f9a3fd
Nadiia-Yuce <soyka9616@gmail.com>
2024-07-13 18:36:13 +0200

    add main logic

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,10 +1,13 @@
-export function getPicturesByQuery(query) {
+import axios from 'axios';
+
+export function getPicturesByQuery({
+  query = '',
+  page = 1,
+  per_page = 15,
+} = {}) {
   const FullURL = `https://pixabay.com/api/?key=44778240-802eb20b46cf6f3dc26aab8d1&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`;
 
-  return fetch(FullURL).then(res => {
-    if (!res.ok) {
-      throw new Error(res.status);
-    }
-    return res.json();
-  });
+  return axios
+    .get(FullURL, { params: { query, page, per_page } })
+    .then(({ data }) => data);
 }
--- src/main.js
+++ src/main.js
@@ -10,20 +10,30 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 const form = document.querySelector('.form');
 const gallery = document.querySelector('.gallery');
 const preloader = document.querySelector('.loader-wrap');
+const loadMoreBtn = document.querySelector('.load-more-btn');
+// const secondPreloader = document.querySelector('.second-loader-wrap');
+
 //Підключення бібліотеки для відображення галереї, що гортається
 const lightbox = new SimpleLightbox('.gallery a', {
   captionsData: 'alt', //Підпис під зображенням
   captionDelay: 250, //Час, після якого буде відображений підпис
 });
+const params = {
+  query: '',
+  page: 1,
+  per_page: 15,
+  max_page: 0,
+};
 
 form.addEventListener('submit', handlerSubmit); //Прослуховувач подій
 
-function handlerSubmit(event) {
+async function handlerSubmit(event) {
   event.preventDefault(); //Запобігаємо дефолтному перезавантаженню сторінки
   gallery.innerHTML = ''; //очищаємо вміст галереї перед новим пошуком
-  const query = form.elements.input.value.toLowerCase().trim(); //Обробка запиту користувача
+  params.page = 1;
+  params.query = form.elements.input.value.trim(); //Обробка запиту користувача
 
-  if (query === '') {
+  if (!params.query) {
     //Якщо користувач залишив поле пустим
     iziToast.warning({
       title: 'Caution',
@@ -38,39 +48,22 @@ function handlerSubmit(event) {
       close: false,
       closeOnClick: true,
     });
+    form.reset();
     return;
   }
 
   preloader.style.display = 'flex'; //Додавання прелоадера
-  getPicturesByQuery(query) //HTTP запит
-    .then(data => {
-      //Якщо об'єкт бекенду data.hits пустий (користувач ввів щось невалідне) -> сповіщуємо про це
-      if (data.hits.length === 0) {
-        iziToast.error({
-          title: 'Error',
-          titleColor: 'white',
-          titleSize: '16px',
-          message:
-            'Sorry, there are no images matching your search query. Please try again!',
-          messageColor: 'white',
-          messageSize: '16px',
-          position: 'bottomRight',
-          backgroundColor: '#ef4040',
-          iconUrl: errorSvg,
-          close: false,
-          closeOnClick: true,
-        });
-      } else {
-        gallery.innerHTML = renderGalleryCard(data.hits); //Виклик функції для створення розмітки
-        lightbox.refresh(); //Метод бібліотеки SimpleLightbox, який видаляє і повторно ініціалізує лайтбокс
-      }
-    })
-    .catch(error => {
+  try {
+    const picture = await getPicturesByQuery(params); //HTTP запит, результатом якого буде об'єкт, записаний в picture
+
+    // Якщо об'єкт бекенду data.hits пустий (користувач ввів щось невалідне) -> сповіщуємо про це
+    if (picture.hits.length === 0) {
       iziToast.error({
         title: 'Error',
         titleColor: 'white',
         titleSize: '16px',
-        message: `Ups... Someting went wrong. Error: ${error}`,
+        message:
+          'Sorry, there are no images matching your search query. Please try again!',
         messageColor: 'white',
         messageSize: '16px',
         position: 'bottomRight',
@@ -79,9 +72,83 @@ function handlerSubmit(event) {
         close: false,
         closeOnClick: true,
       });
-    }) //Інформуємо користувача у разі виникнення помилки
-    .finally(() => {
-      form.reset(); //Оновлення поля форми
-      preloader.style.display = 'none'; //Видалення прелоадера після завантаження картинок
+    } else {
+      params.max_page = Math.ceil(picture.totalHits / params.per_page);
+      if (params.max_page > 1) {
+        loadMoreBtn.style.display = 'block';
+        loadMoreBtn.addEventListener('click', handlerLoadMore);
+      } else {
+        loadMoreBtn.style.display = 'none';
+      }
+      gallery.innerHTML = renderGalleryCard(picture.hits); //Виклик функції для створення розмітки
+      lightbox.refresh(); //Метод бібліотеки SimpleLightbox, який видаляє і повторно ініціалізує лайтбокс
+    }
+    //Інформуємо користувача у разі виникнення помилки
+  } catch (error) {
+    iziToast.error({
+      title: 'Error',
+      titleColor: 'white',
+      titleSize: '16px',
+      message: `Ups... Someting went wrong. Error: ${error}`,
+      messageColor: 'white',
+      messageSize: '16px',
+      position: 'bottomRight',
+      backgroundColor: '#ef4040',
+      iconUrl: errorSvg,
+      close: false,
+      closeOnClick: true,
     });
+  } finally {
+    form.reset(); //Оновлення поля форми
+    preloader.style.display = 'none'; //Видалення прелоадера після завантаження картинок
+  }
+}
+
+async function handlerLoadMore() {
+  params.page += 1;
+  preloader.style.display = 'flex';
+  loadMoreBtn.style.display = 'none';
+  try {
+    const picture = await getPicturesByQuery(params);
+    gallery.insertAdjacentHTML('beforeend', renderGalleryCard(picture.hits));
+    lightbox.refresh();
+
+    //!тут буде плавний скролл
+  } catch (error) {
+    iziToast.error({
+      title: 'Error',
+      titleColor: 'white',
+      titleSize: '16px',
+      message: `Ups... Someting went wrong. Error: ${error}`,
+      messageColor: 'white',
+      messageSize: '16px',
+      position: 'bottomRight',
+      backgroundColor: '#ef4040',
+      iconUrl: errorSvg,
+      close: false,
+      closeOnClick: true,
+    });
+  } finally {
+    preloader.style.display = 'none';
+    if (params.page === params.max_page) {
+      loadMoreBtn.style.display = 'none';
+      loadMoreBtn.removeEventListener('click', handlerLoadMore);
+      iziToast.warning({
+        title: 'Caution',
+        titleColor: 'white',
+        titleSize: '16px',
+        message:
+          'We are sorry, but you have reached the end of search results.',
+        messageColor: 'white',
+        messageSize: '16px',
+        position: 'topRight',
+        backgroundColor: '#ffa000',
+        iconUrl: cautionSvg,
+        close: false,
+        closeOnClick: true,
+      });
+    } else {
+      loadMoreBtn.style.display = 'block';
+    }
+  }
 }
